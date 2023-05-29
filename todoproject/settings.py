08ab6bc36c678daa1c7a9c4329b39f0a1dc1ff52
chaeys2000 <yunseoki0412@daum.net>
2023-05-29 22:26:46 +0900

    1234

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = "django-insecure-(*^42j8m9#xxnowvl&q3pq&*v@b!pzl%4nyw+42z@k8erh=a+n
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+import os
 ALLOWED_HOSTS = ['.ap-northeast-2.compute.amazonaws.com']
 
 
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
